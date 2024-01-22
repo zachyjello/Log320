efaa5b25d1643e90b66e1abafd4b336c0607fa51
zachyjello <110743525+zachyjello@users.noreply.github.com>
2024-01-22 10:21:58 -0500

    Template

---
+++ Board.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+// IMPORTANT: Il ne faut pas changer la signature des méthodes
+// de cette classe, ni le nom de la classe.
+// Vous pouvez par contre ajouter d'autres méthodes (ça devrait 
+// être le cas)
+class Board
+{
+    private Mark[][] board;
+
+    // Ne pas changer la signature de cette méthode
+    public Board() {
+
+    }
+
+    // Place la pièce 'mark' sur le plateau, à la
+    // position spécifiée dans Move
+    //
+    // Ne pas changer la signature de cette méthode
+    public void play(Move m, Mark mark){
+
+
+    }
+
+
+    // retourne  100 pour une victoire
+    //          -100 pour une défaite
+    //           0   pour un match nul
+    // Ne pas changer la signature de cette méthode
+    public int evaluate(Mark mark){
+
+    }
+}
---
+++ CPUPlayer.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+// IMPORTANT: Il ne faut pas changer la signature des méthodes
+// de cette classe, ni le nom de la classe.
+// Vous pouvez par contre ajouter d'autres méthodes (ça devrait 
+// être le cas)
+class CPUPlayer
+{
+
+    // Contient le nombre de noeuds visités (le nombre
+    // d'appel à la fonction MinMax ou Alpha Beta)
+    // Normalement, la variable devrait être incrémentée
+    // au début de votre MinMax ou Alpha Beta.
+    private int numExploredNodes;
+
+    // Le constructeur reçoit en paramètre le
+    // joueur MAX (X ou O)
+    public CPUPlayer(Mark cpu){
+
+    }
+
+    // Ne pas changer cette méthode
+    public int  getNumOfExploredNodes(){
+        return numExploredNodes;
+    }
+
+    // Retourne la liste des coups possibles.  Cette liste contient
+    // plusieurs coups possibles si et seuleument si plusieurs coups
+    // ont le même score.
+    public ArrayList<Move> getNextMoveMinMax(Board board)
+    {
+        numExploredNodes = 0;
+
+    }
+
+    // Retourne la liste des coups possibles.  Cette liste contient
+    // plusieurs coups possibles si et seuleument si plusieurs coups
+    // ont le même score.
+    public ArrayList<Move> getNextMoveAB(Board board){
+        numExploredNodes = 0;
+
+    }
+
+}
